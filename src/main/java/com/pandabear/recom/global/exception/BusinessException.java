96c6fb7a7c47b481ade622433e432d654c18b266
iqpizza6349 <iqpizza6349@gmail.com>
2022-07-19 15:53:56 +0900

    :sparkles: :: (#6) global Exception

---
+++ src/main/java/com/pandabear/recom/global/exception/BusinessException.java
@@ -0,0 +1,14 @@
+package com.pandabear.recom.global.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@AllArgsConstructor
+public class BusinessException extends RuntimeException {
+
+    private HttpStatus status;
+    private String message;
+
+}
