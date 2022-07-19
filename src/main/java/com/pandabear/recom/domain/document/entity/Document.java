f91218ad74c8bef1533216b55a63563bc2c95416
우준성 <woojs0505@naver.com>
2022-07-19 20:01:13 +0900

    :rocket: :: (#7) DocumentService 구현 (#14)

--- src/main/java/com/pandabear/recom/domain/document/entity/Document.java
+++ src/main/java/com/pandabear/recom/domain/document/entity/Document.java
@@ -1,9 +1,11 @@
 package com.pandabear.recom.domain.document.entity;
 
+import com.pandabear.recom.global.exception.BusinessException;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.http.HttpStatus;
 
 import javax.persistence.*;
 
@@ -23,5 +25,15 @@ public class Document {
 
     public void addRecordFile(String recordFileName) {
         this.recordFileName = recordFileName;
+    }
+
+    public void updateContent(String content) {
+        this.content = content;
+    }
+
+    public static class NotExistedException extends BusinessException {
+        public NotExistedException() {
+            super(HttpStatus.NOT_FOUND, "문서를 찾을 수 없습니다.");
+        }
     }
 }
---
+++ src/main/java/com/pandabear/recom/domain/document/ro/DocumentRo.java
@@ -0,0 +1,13 @@
+package com.pandabear.recom.domain.document.ro;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class DocumentRo {
+
+    private String code;
+    private String content;
+    private String recordFileName;
+}
