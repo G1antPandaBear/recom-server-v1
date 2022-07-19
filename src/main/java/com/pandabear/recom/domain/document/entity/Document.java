2c3c48a4c3661332c4890c0fa1d4f0785ba83235
whitebear05 <woojs0505@naver.com>
2022-07-19 10:33:28 +0900

    :sparkles: :: (#2) Document, DocAccessCode entity

--- src/main/java/com/pandabear/recom/domain/document/entity/Document.java
+++ src/main/java/com/pandabear/recom/domain/document/entity/Document.java
@@ -1,12 +1,14 @@
 package com.pandabear.recom.domain.document.entity;
 
 import lombok.Getter;
+import org.springframework.data.redis.core.RedisHash;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
 @Entity(name = "document")
+@RedisHash
 @Getter
 public class Document {
 
--- src/main/java/com/pandabear/recom/global/redis/entity/DocAccessCode.java
+++ src/main/java/com/pandabear/recom/global/redis/entity/DocAccessCode.java
@@ -1,11 +1,20 @@
 package com.pandabear.recom.global.redis.entity;
 
+import lombok.Getter;
+import org.springframework.data.redis.core.RedisHash;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
 @Entity(name = "doc_access_code")
-@Redis
+@RedisHash
+@Getter
 public class DocAccessCode {
 
+    @Id
+    private String id;
 
+    @Column(length = 255, unique = true, nullable = false)
+    private String code;
 }
