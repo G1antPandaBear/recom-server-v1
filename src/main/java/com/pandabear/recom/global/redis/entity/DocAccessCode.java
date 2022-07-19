d06ef05d7f26a9d72d711c6fb974e9aaaed4788c
whitebear05 <woojs0505@naver.com>
2022-07-19 10:28:41 +0900

    :heavy_plus_sign: :: (#2) data-redis dependency

---
+++ src/main/java/com/pandabear/recom/global/redis/entity/DocAccessCode.java
@@ -0,0 +1,11 @@
+package com.pandabear.recom.global.redis.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity(name = "doc_access_code")
+@Redis
+public class DocAccessCode {
+
+
+}
