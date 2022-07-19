1801a550a06fd8c4b63f27cb04328f9777c86212
임재청 <iqpizza6349@gmail.com>
2022-07-19 16:58:39 +0900

    🚀 :: (#9) AccessCodeService 구현 (#11)
    Co-authored-by: whitebear05 <woojs0505@naver.com>

---
+++ src/main/java/com/pandabear/recom/global/redis/service/AccessCodeService.java
@@ -0,0 +1,30 @@
+package com.pandabear.recom.global.redis.service;
+
+import com.pandabear.recom.global.exception.BusinessException;
+import com.pandabear.recom.global.redis.entity.DocAccessCode;
+import com.pandabear.recom.global.redis.repository.DocAccessCodeRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class AccessCodeService {
+
+    private final DocAccessCodeRepository docAccessCodeRepository;
+
+    public DocAccessCode findById(String id) {
+        DocAccessCode docAccessCode = docAccessCodeRepository.findById(id)
+                .orElseThrow(() -> new BusinessException(HttpStatus.NOT_FOUND, "문서 코드를 찾을 수 없습니다."));
+        return docAccessCode;
+    }
+
+    public DocAccessCode create(long documentId) {
+        DocAccessCode docAccessCode = new DocAccessCode(null, documentId);
+        return docAccessCodeRepository.save(docAccessCode);
+    }
+
+}
