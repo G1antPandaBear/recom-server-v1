2eed38145c80c8a55b952de67c80b7081209e479
whitebear05 <woojs0505@naver.com>
2022-07-19 16:15:42 +0900

    :pencil2: :: (#7) 파일 생성

---
+++ src/main/java/com/pandabear/recom/domain/document/service/DocumentService.java
@@ -0,0 +1,7 @@
+package com.pandabear.recom.domain.document.service;
+
+import com.pandabear.recom.domain.document.entity.Document;
+
+public interface DocumentService {
+
+}
---
+++ src/main/java/com/pandabear/recom/domain/document/service/DocumentServiceImpl.java
@@ -0,0 +1,5 @@
+package com.pandabear.recom.domain.document.service;
+
+public class DocumentServiceImpl implements DocumentService {
+
+}
