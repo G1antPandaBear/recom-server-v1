9ae420456dfa3486da3d0cb5a2ca9f2d2c7a9926
iqpizza6349 <iqpizza6349@gmail.com>
2022-07-20 09:30:13 +0900

    :sparkles: :: (#16) md(markdown) export file

--- src/main/java/com/pandabear/recom/global/util/MdFileUtil.java
+++ src/main/java/com/pandabear/recom/global/util/MdFileUtil.java
@@ -4,11 +4,13 @@ import com.pandabear.recom.domain.document.entity.Document;
 import com.pandabear.recom.domain.document.ro.Content;
 import com.pandabear.recom.global.config.AppProperties;
 import lombok.RequiredArgsConstructor;
-import org.docx4j.openpackaging.exceptions.Docx4JException;
-import org.docx4j.openpackaging.packages.WordprocessingMLPackage;
+import net.steppschuh.markdowngenerator.text.Text;
+import net.steppschuh.markdowngenerator.text.emphasis.BoldText;
 
-import java.io.File;
+import java.io.BufferedOutputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.util.List;
 
 @RequiredArgsConstructor
@@ -20,18 +22,23 @@ public class MdFileUtil implements FileUtil {
     @Override
     public String parseFileInfo(Document document) throws IOException {
         String path = appProperties.getUploadUri() + "/out.md";
-
-        try {
-            WordprocessingMLPackage wordprocessingMLPackage
-                    = WordprocessingMLPackage.createPackage();
-            List<Content> contents = documentUtil.parseDocuments(document);
-            for (Content content : contents) {
-                wordprocessingMLPackage.getMainDocumentPart()
-                        .addParagraphOfText(content.getContent());
+        StringBuilder stringBuilder = new StringBuilder();
+        List<Content> contents = documentUtil.parseDocuments(document);
+        for (Content content : contents) {
+            if (content.isBold()) {
+                stringBuilder.append(new BoldText(content.getContent()))
+                        .append("\n");
             }
-            wordprocessingMLPackage.save(new File(path));
-        } catch (Docx4JException e) {
-            e.printStackTrace();
+            else {
+                stringBuilder.append(new Text(content.getContent()))
+                        .append("\n");
+            }
+        }
+
+        try (final BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(
+                new FileOutputStream(path)
+        )) {
+            bufferedOutputStream.write(stringBuilder.toString().getBytes(StandardCharsets.UTF_8));
         }
         return path;
     }
